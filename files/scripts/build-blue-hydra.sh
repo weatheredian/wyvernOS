0ad1900662bad9939c007a2891c095bca7a9f937
WeatheredCameron <cameron@weatheredsecurity.com>
2025-11-17 15:50:04 -0500

    test adding blue-hydra build

---
+++ files/scripts/build-blue-hydra.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+apt-get update && DEBIAN_FRONTEND=noninteractive apt-get install -y \
+  bluez \
+  bluez-test-scripts \
+  python3-bluez \
+  python3-dbus \
+  ubertooth \
+  sqlite3 \
+  libsqlite3-dev
+
+sudo apt-get install ruby-dev bundler
+
+git clone https://github.com/ZeroChaos-/blue_hydra.git
+cp -r blue_hydra /src/blue_hydra
+cd /src/blue_hydra
+bundler install
