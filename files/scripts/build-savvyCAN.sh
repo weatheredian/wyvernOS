f30728a45084375979cfb23c688f9b23d27e6173
WeatheredCameron <cameron@weatheredsecurity.com>
2025-12-29 16:48:17 -0500

    Create build-savvyCAN.sh

---
+++ files/scripts/build-savvyCAN.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+apt-get update && DEBIAN_FRONTEND=noninteractive && apt-get install -y \
+  qt5ct \
+  qt5-qmake
+
+git clone https://github.com/collin80/SavvyCAN.git
+mkdir -p /src/ && cp -r SavvyCAN/ /src/
+cd /src/SavvyCAN
+qmake && make
