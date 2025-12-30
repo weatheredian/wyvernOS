625cdf0dd074d21e8022ac8d61998f138a2a4be8
WeatheredCameron <cameron@weatheredsecurity.com>
2025-12-30 12:09:07 -0500

    Update build-savvyCAN.sh

--- files/scripts/build-savvyCAN.sh
+++ files/scripts/build-savvyCAN.sh
@@ -8,16 +8,9 @@ apt-get update && DEBIAN_FRONTEND=noninteractive && apt-get install -y \
   qt5-qmake \
   git
 add-apt-repository universe && apt-get update && DEBIAN_FRONTEND=noninteractive && apt-get install -y \
-  libqt5gui5t64 \
-  libqt5core5t64 \
-  libqt5printsupport5t64 \
-  libqt5qml5 \
-  libqt5serialbus5-dev \
+  qtbase5-dev \
   libqt5serialport5-dev \
-  libqt5widgets5t64 \
-  libqt5help5 \
-  libqt5network5t64 \
-  libqt5opengl5-dev
+  libqt5serialbus5-dev
 
 git clone https://github.com/collin80/SavvyCAN.git
 mkdir -p /src/ && cp -r SavvyCAN/ /src/
