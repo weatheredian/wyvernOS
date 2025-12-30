a11208a630f631d629bd259c8768d5a2a981144c
WeatheredCameron <cameron@weatheredsecurity.com>
2025-12-30 11:41:40 -0500

    add supporting qt packages to script

--- files/scripts/build-savvyCAN.sh
+++ files/scripts/build-savvyCAN.sh
@@ -6,7 +6,17 @@ apt-get update && DEBIAN_FRONTEND=noninteractive && apt-get install -y \
   g++ \
   qt5ct \
   qt5-qmake \
-  git
+  git \
+  libqt5gui5t64 \
+  libqt5core5t64 \
+  libqt5printsupport5t64 \
+  libqt5qml5 \
+  libqt5serialbus5-dev \
+  libqt5serialport5-dev \
+  libqt5widgets5t64 \
+  libqt5help5 \
+  libqt5network5t64 \
+  libqt5opengl5-dev
 
 git clone https://github.com/collin80/SavvyCAN.git
 mkdir -p /src/ && cp -r SavvyCAN/ /src/
